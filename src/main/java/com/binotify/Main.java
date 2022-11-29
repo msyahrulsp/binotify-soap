49668b853bb853d0eed28c1eac00bda1cf7d6c20
msyahrulsp <13520161@std.stei.itb.ac.id>
2022-11-29 16:49:56 +0700

    fix port

--- src/main/java/com/binotify/Main.java
+++ src/main/java/com/binotify/Main.java
@@ -8,9 +8,9 @@ import javax.xml.ws.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            Endpoint.publish("http://0.0.0.0:3002/ws", new Kumaha());
-            Endpoint.publish("http://0.0.0.0:3002/subscription-status", new CheckStatusController());
-            Endpoint.publish("http://0.0.0.0:3002/generate-api-key", new GenerateAPIKeyController());
+            Endpoint.publish("http://0.0.0.0:3003/ws", new Kumaha());
+            Endpoint.publish("http://0.0.0.0:3003/subscription-status", new CheckStatusController());
+            Endpoint.publish("http://0.0.0.0:3003/generate-api-key", new GenerateAPIKeyController());
             System.out.println("Server started");
         } catch (Exception e) {
             e.printStackTrace();
