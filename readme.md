09b1dd8aa147956419d97af7d22ee182f2c5468d
msyahrulsp <13520161@std.stei.itb.ac.id>
2022-12-02 06:09:47 +0700

    feat: add email notification

--- readme.md
+++ readme.md
@@ -1,45 +1,64 @@
 # Binotify SOAP Service
+
 Service yang digunakan oleh binotify rest dan binotify app untuk proses subscription dan lainnya. Terhubung dengan database mysql.
+
 ## Skema Basis Data
+
 ![image](https://user-images.githubusercontent.com/71055612/205102275-7b281994-1dc4-4310-9481-3d1f4e531452.png)  
 Terdapat perubahan skema di atas sebagai berikut:
+
 - Penambahan tabel api_key, digunakan untuk verifikasi api key dari binotify php dan binotify rest.  
-Atribut:
+  Atribut:
+
 ```
   `id` int NOT NULL AUTO_INCREMENT,
   `services` varchar(255) NOT NULL,
   `api_key` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ```
+
 ## Features
+
 ### Logging
+
 Digunakan untuk memasukkan setiap request yang diterima SOAP ke database.  
 Usage:
+
 1. Add code below in your controller.
+
 ```java
 @Resource WebServiceContext wsContext;
 ```
+
 2. Add code below in your interface.
+
 ```java
 @Resource
 ```
+
 3. Add code below to log the incoming request.
+
 ```java
 // void
 insertLog(wsContext, "log message", "endpoint");
 ```
 
 ### API Key
+
 Digunakan untuk verifikasi incoming request yang berkaitan dengan subscription.  
 Usage:
+
 1. Add code below before any operation in your controller.
+
 ```java
 // return boolean
 verifyAPIKey(wsContext)
 ```
+
 2. Do step 1 and 2 in logging above (we need WebServiceContext to read http request headers).
 
 Example for API key verification and logging:
+
 ```java
 public class CheckStatusController extends Database implements CheckStatusInterface {
     @Resource WebServiceContext wsContext; // add to your controller and interface
@@ -70,8 +89,10 @@ public class CheckStatusController extends Database implements CheckStatusInterf
 ```
 
 ## Pembagian Tugas SOAP
-| Task        | NIM           |
-| ------------- |-------------|
-|Request Subscription | 13520080 |
-|Check Request | 13520002 |
-|Accept/Reject Request | 13520161 |
+
+| Task                  | NIM      |
+| --------------------- | -------- |
+| Request Subscription  | 13520080 |
+| Check Request         | 13520002 |
+| Accept/Reject Request | 13520161 |
+| Notifikasi E-mail     | 13520161 |
--- src/main/java/com/binotify/controllers/NewSubscriberController.java
+++ src/main/java/com/binotify/controllers/NewSubscriberController.java
@@ -7,19 +7,38 @@ import javax.annotation.Resource;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.mail.Authenticator;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Message;
+import javax.mail.Multipart;
 import javax.xml.ws.WebServiceContext;
-import java.sql.ResultSet;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+import java.util.Scanner;
 
 @WebService(endpointInterface = "com.binotify.interfaces.NewSubscriberInterface")
 public class NewSubscriberController extends Database implements NewSubscriberInterface {
-    @Resource WebServiceContext wsContext;
+    @Resource
+    WebServiceContext wsContext;
+
     @WebMethod
-    public boolean newSubscribe(@WebParam(name = "creator_id") int creator_id, @WebParam(name = "subscriber_id") int subscriber_id) {
+    public boolean newSubscribe(@WebParam(name = "creator_id") int creator_id,
+            @WebParam(name = "subscriber_id") int subscriber_id) {
         if (verifyAPIKey(wsContext)) {
-            String query = "insert into subscription (creator_id, subscriber_id, status) values ('" + creator_id + "', '" + subscriber_id + "', 'PENDING')";
+            String query = "insert into subscription (creator_id, subscriber_id, status) values ('" + creator_id
+                    + "', '" + subscriber_id + "', 'PENDING')";
             try {
                 int res = this.executeUpdate(query);
                 if (res != 0) {
+                    this.sendEmail();
                     insertLog(wsContext, "Mendapatkan data subscription baru", "/subscribe");
                     return true;
                 }
@@ -31,6 +50,129 @@ public class NewSubscriberController extends Database implements NewSubscriberIn
             }
         } else {
             return false;
+        }
+    }
+
+    public String getAdmin() {
+        try {
+            URL url = new URL(System.getenv("REST_API_URL") + "/admin");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed :HTTP error code :" + conn.getResponseCode());
+            }
+
+            String inline = "";
+            Scanner scanner = new Scanner(url.openStream());
+
+            while (scanner.hasNext()) {
+                inline += scanner.nextLine();
+            }
+
+            scanner.close();
+            conn.disconnect();
+
+            String res = "";
+            int emailStartIdx = inline.indexOf("email") + 8;
+            for (int i = emailStartIdx; inline.charAt(i) != ','; i++) {
+                if (inline.charAt(i) != '"') {
+                    res += inline.charAt(i);
+                }
+            }
+            res += "|";
+            int nameStartIdx = inline.indexOf("name") + 7;
+            for (int i = nameStartIdx; inline.charAt(i) != '}'; i++) {
+                if (inline.charAt(i) != '"') {
+                    res += inline.charAt(i);
+                }
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+
+            // Fallback Email
+            return "admin@gmail.com";
+        }
+    }
+
+    public void sendEmail() {
+        Properties prop = new Properties();
+        if (System.getenv("MAIL_MODE").equals("dev")) {
+            prop.put("mail.smtp.auth", true);
+            prop.put("mail.smtp.starttls.enable", "true");
+            prop.put("mail.smtp.host", "smtp.mailtrap.io");
+            prop.put("mail.smtp.port", "2525");
+            prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+        } else {
+            prop.put("mail.smtp.host", "smtp.gmail.com");
+            prop.put("mail.smtp.port", "465");
+            prop.put("mail.smtp.auth", true);
+            prop.put("mail.smtp.socketFactory.port", "465");
+            prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        }
+
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                if (System.getenv("MAIL_MODE").equals("dev")) {
+                    return new PasswordAuthentication("b420a2e34fcc6a", "479fc24e1e2b1a");
+                } else {
+                    return new PasswordAuthentication("mssp892@gmail.com", System.getenv("MAIL_PASSWORD"));
+                }
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        try {
+            String adminCred = this.getAdmin();
+            String adminEmail = adminCred.split("\\|")[0];
+            String adminName = adminCred.split("\\|")[1];
+
+            message.setFrom(new InternetAddress("no-reply@binotify.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(adminEmail));
+            message.setSubject("[Binotify Premium] New Subscription");
+
+            String text = "<table border='0' cellpadding='0' cellspacing='0' width='100%' style='border-collapse:collapse; padding:0; margin-top:20px;'>"
+                    +
+                    "<tr>" +
+                    "<td align='center'>" +
+                    "<table style='border: 0; width: 100%;'>" +
+                    "<tr style='border: 0;'>" +
+                    "<img src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png'  width='200px' />"
+                    +
+                    "</tr>" +
+                    "<tr style='border: 0;'>" +
+                    "<h2 style='width:250px; margin-top: 10px'>New Subscription Request</h2> " +
+                    "</tr>" +
+                    "<tr style='border: 0;'>" +
+                    "<div style='width:400px;'>Hi " + adminName
+                    + ", it looks like there's someone who makes a new subscription request. Don't forget to check them out!</div>"
+                    +
+                    "</tr>" +
+                    "<tr style='border: 0;'>" +
+                    "<a href='http://localhost:3000/subscription' target='_blank' style='cursor:pointer;'>" +
+                    "<button style='width:200px; height:40px; background-color:#1DB954; color:white; border:none; border-radius:5px; margin-top:25px; cursor:pointer;'>Check Now</button>"
+                    +
+                    "</a>" +
+                    "</tr>" +
+                    "</table>" +
+                    "</td>" +
+                    "</tr>" +
+                    "</table>";
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(text, "text/html; charset=utf-8");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            message.setContent(multipart);
+            Transport.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
         }
     }
 
